b009f9a54ff4f7989f3b49577510a50fb98e7487
UltimaPhoenix <2079222+UltimaPhoenix@users.noreply.github.com>
2024-06-16 23:09:37 +0200

    new features plugin

--- src/main/groovy/io/github/ultimaphoenix/gradleversionplugin/VersionPlugin.groovy
+++ src/main/groovy/io/github/ultimaphoenix/gradleversionplugin/VersionPlugin.groovy
@@ -3,22 +3,73 @@ package io.github.ultimaphoenix.gradleversionplugin
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
+enum UpdateType {
+    MAJOR, MINOR, PATCH
+}
+
 class VersionPlugin implements Plugin<Project> {
+
+    private void updateProjectVersion(String newVersion) {
+        File buildFile = project.file("build.gradle")
+        String content = buildFile.text
+
+        content = content.replaceAll(/^version *= *['"].*['"]/, 'version = "' + newVersion + '"')
+
+        buildFile.text = content
+
+        println "Version updated to: $newVersion"
+    }
+
+    private String computeVersion(UpdateType updateType) {
+        def versionParts = project.version.split('\\.')
+        def major = versionParts[0].toInteger()
+        def minor = versionParts[1].toInteger()
+        def patch = versionParts[2].toInteger()
+        def suffix = versionParts.size() > 3 ? versionParts[3] : ""
+
+        (major, minor, patch) = switch (updateType) {
+            case UpdateType.MAJOR -> [major + 1, 0, 0]
+            case UpdateType.MINOR -> [major, minor + 1, 0]
+            case UpdateType.PATCH -> [major, minor, patch + 1]
+        }
+
+        return suffix ? "${major}.${minor}.${patch}.${suffix}" : "${major}.${minor}.${patch}"
+    }
+
     void apply(Project project) {
         project.tasks.register('updateVersion') {
             group = "Release"
             description = "Update the project version to the specified."
             doLast {
                 String newVersion = project.findProperty("newVersion") ?: '1.0.0' // Default version if not specified
+                updateProjectVersion(newVersion)
+            }
+        }
 
-                File buildFile = project.file("build.gradle")
-                String content = buildFile.text
+        project.tasks.register('bumpMajor') {
+            group = "Release"
+            description = "Bump the Major release of the project."
+            doLast {
+                String newVersion = computeNewVersion(UpdateType.MAJOR)
+                updateProjectVersion(newVersion)
+            }
+        }
 
-                content = content.replaceAll(/^version *= *['"].*['"]/, 'version = "' + newVersion + '"')
+        project.tasks.register('bumpMinor') {
+            group = "Release"
+            description = "Bump the Minor release of the project."
+            doLast {
+                String newVersion = computeNewVersion(UpdateType.MINOR)
+                updateProjectVersion(newVersion)
+            }
+        }
 
-                buildFile.text = content
-
-                println "Version updated to: $newVersion"
+        project.tasks.register('bumpPatch') {
+            group = "Release"
+            description = "Bump the Patch release of the project."
+            doLast {
+                String newVersion = computeNewVersion(UpdateType.PATCH)
+                updateProjectVersion(newVersion)
             }
         }
 
